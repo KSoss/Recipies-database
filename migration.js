8d06a2f7580248df35951e1160fa360e720427b1
Never404s <kdsossa@gmail.com>
2023-03-06 21:32:10 -0600

    fixed some plural issues

--- migration.js
+++ migration.js
@@ -49,8 +49,8 @@ function createJoinTables() {
   pool.query(
     `CREATE TABLE recipes_ingredients (
       id SERIAL PRIMARY KEY,
-      recipe_id INTEGER REFERENCES recipes(id) ON DELETE CASCADE,
-      ingredient_id INTEGER REFERENCES ingredients(id) ON DELETE CASCADE
+      recipes_id INTEGER REFERENCES recipes(id) ON DELETE CASCADE,
+      ingredients_id INTEGER REFERENCES ingredients(id) ON DELETE CASCADE
     );`, (err, result) => {
       if (err) {
         console.error('Error making recipes_ingredients:', err);
@@ -63,8 +63,8 @@ function createJoinTables() {
   pool.query(
     `CREATE TABLE recipes_tags (
       id SERIAL PRIMARY KEY,
-      recipe_id INTEGER REFERENCES recipes(id) ON DELETE CASCADE,
-      tag_id INTEGER REFERENCES tags(id) ON DELETE CASCADE  
+      recipes_id INTEGER REFERENCES recipes(id) ON DELETE CASCADE,
+      tags_id INTEGER REFERENCES tags(id) ON DELETE CASCADE  
     );`, (err, result) => {
       if (err) {
         console.error('Error making recipes_tags:', err);
